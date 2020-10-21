409ffb6659514b98f96fb40d030952d87288c858
Patrick Chin <60618726+patchinco@users.noreply.github.com>
2020-10-21 21:27:49 +0200

    Add IIS Deployment

--- .teamcity/JavaScript/buildTypes/JavaScript_03DeployToStaging.kt
+++ .teamcity/JavaScript/buildTypes/JavaScript_03DeployToStaging.kt
@@ -2,6 +2,7 @@ package JavaScript.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 
 object JavaScript_03DeployToStaging : BuildType({
     uuid = "2cef2f34-f9dd-4bd8-871d-a4afc592772f"
@@ -10,6 +11,17 @@ object JavaScript_03DeployToStaging : BuildType({
     vcs {
         root(JavaScript.vcsRoots.JavaScript_HttpsGithubComG0t4teamcityCourseCards)
     }
+	
+	steps {
+        script {
+            name = "IIS Deploy"
+            id = "RUNNER_6"
+            scriptContent = """
+		rmdir /S /Q \inetpub\wwwroot
+		xcopy /S /I /Y app \inetpub\wwwroot\
+			"""
+        }
+    }  
 
     dependencies {
         snapshot(JavaScript_02Chrome) {
